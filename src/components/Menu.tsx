5c8e8292bab76abba5dc73df5c2bd0686565a3f5
Christopher Chi <chi.christopher109@gmail.com>
2024-09-17 00:31:52 -0400

    Dashboard-Initial
    Added sectional components of layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,26 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((item) => (
+        <div key={item.title} className="flex flex-col gap-2">
+          <span className="hidden lg:block text-gray-400 font-light my-4">{item.title}</span>
+
+          {item.items.map((selfItem) => (
+            <Link href={selfItem.href} key={selfItem.label} className="flex items-center justify-center lg:justify-start 
+            gap-4 text-gray-500 py-2">
+              <Image src={selfItem.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{selfItem.label}</span>
+            </Link>
+          ))}
+
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
